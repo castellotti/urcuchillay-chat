7ebada180da1f315033809a7d53ad8dbe52276c4
Steve Castellotti <sc@jigsaw.studio>
2024-01-22 03:33:50 -0800

    Updated default system prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "You are Urcuchillay, a web chat interface for large language models. Follow the user's instructions carefully. Respond using markdown.";
 
 export const API_HOST =
   process.env.API_HOST || 'https://api.openai.com';
@@ -10,6 +10,9 @@ export const AZURE_DEPLOYMENT_ID =
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+
+export const OPENAI_API_KEY =
+    process.env.OPENAI_API_KEY || 'xxxxxxxx';
 
 export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';

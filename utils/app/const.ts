650798c60ac4a4210ecad769a82475786a899696
Steve Castellotti <sc@jigsaw.studio>
2024-02-15 11:18:34 -0800

    added context-only prompt, set default temperature to 0, and updated OpenAI API version

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -2,6 +2,13 @@ export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
   "You are Urcuchillay, a web chat interface for large language models. Follow the user's instructions carefully. Respond using markdown.";
 
+export const CONTEXT_ONLY_PROMPT =
+  process.env.CONTEXT_ONLY_PROMPT ||
+  "You are Urcuchillay, a web chat interface for large language models. Follow the user's instructions carefully. Respond using markdown.\n" +
+    "Always answer the query using the provided context information, and not prior knowledge. Some rules to follow:\n" +
+    "1. Never directly reference the given context in your answer.\n" +
+    "2. Avoid statements like 'Based on the context, ...' or 'The context information ...' or anything along those lines.";
+
 export const API_HOST =
   process.env.API_HOST || 'https://api.openai.com';
 
@@ -9,7 +16,7 @@ export const AZURE_DEPLOYMENT_ID =
     process.env.AZURE_DEPLOYMENT_ID || '';
 
 export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0");
 
 export const OPENAI_API_KEY =
     process.env.OPENAI_API_KEY || 'xxxxxxxx';
@@ -18,7 +25,7 @@ export const OPENAI_API_TYPE =
   process.env.OPENAI_API_TYPE || 'openai';
 
 export const OPENAI_API_VERSION =
-  process.env.OPENAI_API_VERSION || '2023-03-15-preview';
+  process.env.OPENAI_API_VERSION || '2023-09-01-preview';
 
 export const OPENAI_ORGANIZATION =
   process.env.OPENAI_ORGANIZATION || '';

5aa62e15c19ddd3b0d642663847aab21c4053b3f
Steve Castellotti <sc@jigsaw.studio>
2024-02-15 11:20:16 -0800

    welcome page cleanup

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -347,35 +347,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             Welcome to Urcuchillay Chat
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Urcuchillay Chat Web-based chat interface in the style of OpenAI's ChatGPT UI.`}</div>
-            <div className="mb-2 font-bold">
-              Important: Urcuchillay Chat is 100% unaffiliated with OpenAI.
-            </div>
-          </div>
-          <div className="text-center text-gray-500 dark:text-gray-400">
-            <div className="mb-2">
-              Urcuchillay Chat allows you to plug in your API key to use this UI with their API.
-            </div>
-            <div className="mb-2">
-              It is <span className="italic">only</span> used to communicate
-              with their API.
-            </div>
-            <div className="mb-2">
-              {t(
-                'Please set your OpenAI API key in the bottom left of the sidebar.',
-              )}
-            </div>
-            <div>
-              {t("If you don't have an OpenAI API key, you can get one here: ")}
-              <a
-                href="https://platform.openai.com/account/api-keys"
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                openai.com
-              </a>
-            </div>
+            <div className="mb-8">{`Urcuchillay Chat is a web-based chat interface in the style of OpenAI's ChatGPT UI.`}</div>
           </div>
         </div>
       ) : modelError ? (

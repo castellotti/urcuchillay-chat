cb5b64b54883edb19775c6f8dbfbaca9b8a3577d
Steve Castellotti <sc@jigsaw.studio>
2024-02-15 20:09:30 -0800

    updated supported models, maxLength, and tokenLimits

--- types/openai.ts
+++ types/openai.ts
@@ -8,10 +8,11 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5_16K = 'gpt-35-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
+  GPT_3_5 = 'gpt-3.5-turbo',
   GPT_4_32K = 'gpt-4-32k',
+  GPT_4 = 'gpt-4',
   LLAMA_2_7B = 'llama-2-7b-chat',
   LLAMA_2_13B = 'llama-2-13b-chat',
   LLAMA_2_70B = 'llama-2-70b-chat',
@@ -26,15 +27,21 @@ export const fallbackModelID = OpenAIModelID.MISTRAL_7B;
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
+    name: 'GPT-3.5 Turbo',
     maxLength: 12000,
     tokenLimit: 4000,
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
+    name: 'GPT-3.5 Turbo',
     maxLength: 12000,
     tokenLimit: 4000,
+  },
+  [OpenAIModelID.GPT_3_5_16K]: {
+    id: OpenAIModelID.GPT_3_5_16K,
+    name: 'GPT-3.5 Turbo 16k',
+    maxLength: 48000,
+    tokenLimit: 16000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
@@ -69,13 +76,13 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.MISTRAL_7B]: {
     id: OpenAIModelID.MISTRAL_7B,
     name: 'Mistral 7B',
-    maxLength: 12000,
-    tokenLimit: 4000,
+    maxLength: 24000,
+    tokenLimit: 8000,
   },
   [OpenAIModelID.MIXTRAL_8X7B]: {
     id: OpenAIModelID.MIXTRAL_8X7B,
     name: 'Mixtral 8x7B',
-    maxLength: 32000,
+    maxLength: 96000,
     tokenLimit: 32000,
   },
   [OpenAIModelID.PHI_2]: {
